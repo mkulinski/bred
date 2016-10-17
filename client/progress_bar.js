a9ac82478722389a54c33b25e75ab1485186e658
Michael Kulinski <me@michaelkulinski.com>
2016-10-16 17:14:26 -0700

    finished converting progress_bar component to stateless. Also this linting thing should have been done a long time ago. These victory tags are still a little wonky

--- client/progress_bar.js
+++ client/progress_bar.js
@@ -1,55 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { VictoryStack,VictoryBar } from 'victory';
+import { VictoryStack, VictoryBar } from 'victory';
 
-
-export default class ProgressBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  
-  render() {
-    console.log('progressBar props',this.props);
-    const testExpenses = this.props.expenses
-    let filler = ""
-    let totalExpense = function reduce(array) {
-        let total = 0;
-        for (var i = array.length; i--;) {
-        total +=  parseInt(array[i].amount);
-        }
-        return total;
+const ProgressBar = props => {
+  const testExpenses = props.expenses;
+  const totalExpense = function reduce(array) {
+    let total = 0;
+    for (let i = array.length; i--;) {
+      total += parseInt(array[i].amount);
     }
-    let totalExp = totalExpense(testExpenses)
+    return total;
+  };
+  const totalExp = totalExpense(testExpenses);
+  const income = parseInt(props.income);
 
-    let income = parseInt(this.props.income);
+  return (
+    <div className="progress">
+      <VictoryStack
+        horizontal
+        height={50}
+        padding={25}
+        style={{
+          data: { width: 22 },
+          labels: { fontSize: 20 },
+        }}
+      >
+        <VictoryBar
+          style={{ data: { fill: 'tomato' } }}
+          data={[
+          { x: 1, y: totalExp },
+          ]}
+        />
+        <VictoryBar
+          style={{ data: { fill: 'blue' } }}
+          data={[
+          { x: 1, y: income },
+          ]}
+        />
+      </VictoryStack>
+    </div>
+    );
+};
 
-    console.log('income', typeof income)
-    console.log('totalExp',totalExp)
-
-    return (
-       <div className="progress">
-       <VictoryStack horizontal
-         height={50}
-         padding={25}
-         style={{
-           data: {width: 22},
-           labels: {fontSize: 20}
-         }}
-       >
-         <VictoryBar
-           style={{data: {fill: "tomato"}}}
-           data={[
-             {x: 1, y: totalExp}
-           ]}
-         />
-         <VictoryBar
-           style={{data: {fill: "blue"}}}
-           data={[
-             {x: 1, y: income}
-           ]}
-         />
-       </VictoryStack>
-        </div>
-    )
-  }
-}
+export default ProgressBar;
