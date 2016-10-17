410b2cd36bab210273fa4b7e2e2e3b3cd421c4b3
Michael Kulinski <me@michaelkulinski.com>
2016-10-16 17:01:26 -0700

    fixed syntax on dashboard owned components and linted the f out of them.

--- client/time_chart.js
+++ client/time_chart.js
@@ -1,41 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { VictoryStack,VictoryChart,VictoryAxis,VictoryBar } from 'victory';
+import { VictoryStack, VictoryChart, VictoryAxis, VictoryBar } from 'victory';
 
 const TimeChart = props => {
-      let cats =[];
-      let dataPoints = []
-        let testExpenses = props.expenses;
-        let filler = ""
+  let cats = [];
+  let dataPoints = [];
+  const testExpenses = props.expenses;
+  let filler = '';
 
-        for (let i = 0; i< testExpenses.length;i++) {
-          let costTime ={}
-          if (i % 2 === 0 || i === 0) {
-            filler="blue"
-          } else {
-            filler="tomato"
-          }
-          cats.push(testExpenses[i].category);
-          costTime = {x:new Date(testExpenses[i].createdAt).getSeconds(), y:parseInt(testExpenses[i].amount), fill: filler}
-          dataPoints.push(costTime);
-        }
+  for (let i = 0; i < testExpenses.length; i++) {
+    let costTime = {};
+    if (i % 2 === 0 || i === 0) {
+      filler = 'blue';
+    } else {
+      filler = 'tomato';
+    }
+    cats.push(testExpenses[i].category);
+    costTime = {
+      x: new Date(testExpenses[i].createdAt).getSeconds(),
+      y: parseInt(testExpenses[i].amount), fill: filler,
+    };
+    dataPoints.push(costTime);
+  }
 
-    return (
-      <div className="time">
+  return (
+    <div className="time">
       <VictoryChart
-  height={500}
-  domainPadding={{x: 220}}>
-  <VictoryStack
-    labels={cats}
-    colorScale={"qualitative"}
-  >
-    <VictoryBar
-      data={dataPoints}
-    />
-  </VictoryStack>
-</VictoryChart>
-</div>
-    )
-}
+        height={500}
+        domainPadding={{ x: 220 }}
+      >
+        <VictoryStack
+          labels={cats}
+          colorScale={"qualitative"}
+        >
+          <VictoryBar
+            data={dataPoints}
+          />
+        </VictoryStack>
+      </VictoryChart>
+    </div>
+  );
+};
 
 export default TimeChart;
