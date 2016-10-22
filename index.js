44427212bdc551d73a80b91f827abd2df2f6ca15
Michael Kulinski <me@michaelkulinski.com>
2016-10-22 15:25:59 -0700

    fixed some of the syntax issues in index.js file

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 import ReactDOM from 'react-dom';
-import DashBoard from './client/dashboard'
-import Login from './client/login'
-import Signup from './client/signup'
-import Profile from './client/profile'
-import Nav from './client/nav';
+import DashBoard from './client/dashboard';
+import Login from './client/login';
+import Signup from './client/signup';
+import Profile from './client/profile';
 import Container from './Container';
-import Expense from './client/expense'
-import Home from './client/home'
-
+import Expense from './client/expense';
+import Home from './client/home';
 
 
 ReactDOM.render((
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={Container}>
-        <IndexRoute component={Home}/>
+        <IndexRoute component={Home} />
         <Route path="/dashboard" component={DashBoard} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
