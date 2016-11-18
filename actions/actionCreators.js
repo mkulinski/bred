f6909662d271943bca19eade374bf34941ba05c0
Michael Kulinski <me@michaelkulinski.com>
2016-11-18 11:57:04 -0800

    took out some useless comments and refactored the getUserExpenses and sendLoginData

--- actions/actionCreators.js
+++ actions/actionCreators.js
@@ -1,45 +1,38 @@
-// what passes through props and you call in react
 import axios from 'axios';
 import { push } from 'react-router-redux';
 
-export function changeUserInfo(username, email){
+export function changeUserInfo(username, email) {
   return {
     type: 'CHANGE_USER_INFO',
     username,
-    email
-  }
+    email,
+  };
 }
 
 function getUserExpenses(username) {
   return dispatch => {
     axios.get(`http://localhost:3000/api/user/${username}/expense`)
     .then(response => {
-      console.log('user- ', username,'expeses = ', response);
       dispatch({ type: 'SET_USER_EXPENSES', payload: response.data });
     })
-    .then (() => {
+    .then(() => {
       dispatch(push('/dashboard'));
     })
-    .catch( err => {
-      console.log('err', err);
-    })
-  }
+    .catch(err => console.log('err', err));
+  };
 }
 
-export function sendLoginData(userObj){
+export function sendLoginData(userObj) {
   return dispatch => {
     axios.post('http://localhost:3000/login', userObj)
     .then(response => {
-      console.log('userObj that I get back = ', response.data)
-      dispatch({type: 'SEND_LOGIN_DATA_SUCCESS', payload: response.data})
+      dispatch({ type: 'SEND_LOGIN_DATA_SUCCESS', payload: response.data });
     })
     .then(() => {
       dispatch(getUserExpenses(userObj.username));
     })
-    .catch( err => {
-      console.log('hit the error in sendLoginData')
-      console.log('err', err)
-      dispatch({type: 'SEND_LOGIN_DATA_ERROR', error: err})
-    })
-  }
+    .catch(err => {
+      dispatch({ type: 'SEND_LOGIN_DATA_ERROR', error: err });
+    });
+  };
 }
--- reducer.js
+++ reducer.js
@@ -1,48 +1,42 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-
 function user(state = {}, action) {
-
   switch (action.type) {
     case 'SEND_LOGIN_DATA_SUCCESS':
       return Object.assign({}, state, {
         username: action.payload.username,
         income: action.payload.income,
         loading: false,
-        error: null
-      })
-      break;
+        error: null,
+      });
     case 'SEND_LOGIN_DATA_ERROR':
       return Object.assign({}, state, {
         username: '',
         loading: false,
-        error: action.err
-      })
-      break;
+        error: action.err,
+      });
     case 'CHANGE_USER_INFO':
       return Object.assign({}, state, {
         username: action.username,
-        email: action.email
-      })
-      break;
+        email: action.email,
+      });
     default:
       return state;
   }
 }
 
-function expenses(state=[], action) {
+function expenses(state = [], action) {
   switch (action.type) {
     case 'SET_USER_EXPENSES':
       return Object.assign({}, state, {
         expenses: action.payload,
-      })
-      break;
-      default:
-        return state;
+      });
+    default:
+      return state;
   }
 }
 
-const userReducer = combineReducers({user, expenses, routing: routerReducer });
+const userReducer = combineReducers({ user, expenses, routing: routerReducer });
 
 export default userReducer;
