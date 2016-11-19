00dbf27061e320772f94329d25cc7ec6c8336a32
Michael Kulinski <me@michaelkulinski.com>
2016-11-19 10:18:46 -0800

    fixed some syntax issues so the linter would stop yelling at me

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './client')));
 
-app.get('/', (req, res) =>{
-	res.sendFile(path.join(__dirname, './../index.html'))
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, './../index.html'));
 });
 
 app.post('/login',
